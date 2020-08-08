764a0a264af7ffcc8d216228429eca9a55d33586
Zavrazhneva <zavrazhneva.a@gmail.com>
2020-08-08 13:22:19 +0300

    add folder for github pages

---
+++ docs/precache-manifest.8e03b5b1fdc656bdb5271d975252691e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c544ea60d4c29113eaed5a21f8b24d78",
+    "url": "/index.html"
+  },
+  {
+    "revision": "760d30ee52c9167a0370",
+    "url": "/static/css/main.85706086.chunk.css"
+  },
+  {
+    "revision": "43a8cbb0767d06a4b3f7",
+    "url": "/static/js/2.44c3493e.chunk.js"
+  },
+  {
+    "revision": "5e9b6f5c16b720e8a86875f97734673e",
+    "url": "/static/js/2.44c3493e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "760d30ee52c9167a0370",
+    "url": "/static/js/main.ff950155.chunk.js"
+  },
+  {
+    "revision": "80e28a5f48e3db954cc0",
+    "url": "/static/js/runtime-main.f1e1491c.js"
+  }
+]);
