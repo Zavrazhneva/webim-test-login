6a51d631701d839dc7d06c0403e01b8df6782e7b
Zavrazhneva <zavrazhneva.a@gmail.com>
2020-08-08 13:31:33 +0300

    commit missed files

---
+++ docs/precache-manifest.85052be4f082824ab737466d5c35fab5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5fbd8dc3ea53ae41ff7b90641aac2d19",
+    "url": "/webim-test-login/index.html"
+  },
+  {
+    "revision": "44cf51204c8c68f3a8d2",
+    "url": "/webim-test-login/static/css/main.85706086.chunk.css"
+  },
+  {
+    "revision": "43a8cbb0767d06a4b3f7",
+    "url": "/webim-test-login/static/js/2.44c3493e.chunk.js"
+  },
+  {
+    "revision": "5e9b6f5c16b720e8a86875f97734673e",
+    "url": "/webim-test-login/static/js/2.44c3493e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "44cf51204c8c68f3a8d2",
+    "url": "/webim-test-login/static/js/main.42a8782c.chunk.js"
+  },
+  {
+    "revision": "bafede92e1044126659e",
+    "url": "/webim-test-login/static/js/runtime-main.f945fbc1.js"
+  }
+]);
